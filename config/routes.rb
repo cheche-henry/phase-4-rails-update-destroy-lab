ca55112389ad0aab7d8a93a83c971bb97adc051b
henry cheche <henry.cheche@student.moringaschool.com>
2023-06-26 08:27:28 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  # Other routes...
+
+  resources :plants, only: [:index, :show, :create, :update, :destroy]
+  # Add the above line if you haven't already defined the resource route for plants.
+
+  patch '/plants/:id', to: 'plants#update' # Route for updating a specific plant
+  delete '/plants/:id', to: 'plants#destroy' # Route for deleting a specific plant
 end
